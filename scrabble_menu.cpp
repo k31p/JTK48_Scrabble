aed0d3536076e74ca74f128e453ab1379b3e03e4
Yobel D <ryobeld@gmail.com>
2023-12-09 21:55:13 +0700

    add credit and how to play

--- scrabble_menu.cpp
+++ scrabble_menu.cpp
@@ -37,6 +37,29 @@ void createMenus(char menus[5][50], int startPos){
     }
 }
 
+void HowToPlay(){  
+    tc_clear_screen();  
+    printf("%s How To Play\n%s",TC_YEL,TC_NRM); 
+    printf("%s1. Pemain menentukan banyak pemain, kemudian set pemain\n%s",TC_YEL,TC_NRM);  
+    printf("%s2. Saat permainan dimulai, pemain pertama harus meletakkan huruf tepat di tengah papan\n%s",TC_YEL,TC_NRM);  
+    printf("%s3. Pemain memasukkan kata, kemudian tentukan koordinat sesuai dengan tempat meletakkannya\n%s",TC_YEL,TC_NRM);  
+    printf("%s4. Pemain akan mendapat skor sesuai dengan kata yang dimasukkan\n%s",TC_YEL,TC_NRM);  
+    printf("%s5. Setelah pemain meletakkan kata atau kehabisan waktu untuk meletakkan kata , maka giliran pemain habis dan beralih ke giliran selanjutnya\n%s",TC_YEL,TC_NRM);
+    printf("%s6. Permainan berlanjut sampai giliran pemain telah mencapai 25 atau huruf yang dimiliki dan yang dikantung player telah habis maka permainan dianggap selesai\n%s",TC_YEL,TC_NRM);
+    printf("%s7. Peringkat pemain akan terlihat mengurut dari tertinggi ke terendah, pemain yang memiliki skor paling tinggi adalah pemenangnya ^-^\n%s",TC_YEL,TC_WHT);
+}
+
+void Credit(){
+    tc_clear_screen();
+    printf("%s",TC_RED);
+    centerPos("Made by JTK48");
+    centerPos("Member's Group");
+    centerPos("1. Yobel El'Roy Doloksaribu - 231524029");
+    centerPos("2. Amr Fadhilah Abiyyu Alif Basysyar - 231524002");
+    centerPos("3. Micho Dhani Firmansyah - 231524013");
+    printf("%s",TC_WHT);
+}
+
 void selectMenu(){
     switch(globalMenuChoice){
         case 1:
@@ -44,8 +67,10 @@ void selectMenu(){
         case 2:
             break;
         case 3:
+            HowToPlay();
             break;
         case 4:
+            Credit();
             break;
         case 5:
             tc_clear_screen();
@@ -64,6 +89,18 @@ void switchMenu(char menus[5][50], int menuPos){
         }
         createMenus(menus, menuPos);
     } else if(input == 's' || input == 'S'){
+        globalMenuChoice++;
+        if(globalMenuChoice > 5){
+            globalMenuChoice = 1;
+        }
+        createMenus(menus, menuPos);
+    } else if(input == 'a' || input == 'A'){
+        globalMenuChoice--;
+        if(globalMenuChoice < 1){
+            globalMenuChoice = 5;
+        }
+        createMenus(menus, menuPos);
+    } else if(input == 'd' || input == 'D'){
         globalMenuChoice++;
         if(globalMenuChoice > 5){
             globalMenuChoice = 1;
