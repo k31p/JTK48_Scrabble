6d9bbb5a9c2f602e73ad90c92972e844a97865ed
amrfad <amr162005@gmail.com>
2023-12-09 20:59:36 +0700

    memperbaiki modul timer

--- scrabble_timer.h
+++ scrabble_timer.h
@@ -1,13 +1,16 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-#include <stdio.h>
-#include <conio.h>
-#include <time.h>
-#include <unistd.h>
-#include <string.h>
+#include <stdio.h> 
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-void countdown();
+extern bool stop_thread;
+extern int timer;
+void initTimer();
+void* countdown_thread(void* arg);
+void startCountdown();
 
 #endif
